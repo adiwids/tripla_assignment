d7427862041fb4ca35e0c3de2a8db79da56af13f
Adi Widyawan <day4die@gmail.com>
2023-04-13 20:58:01 +0700

    Add following relations on user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :sleep_cycles, dependent: :destroy
+  with_options(class_name: 'Following', dependent: :destroy) do
+    has_many :follow_relations, foreign_key: 'follower_id'
+    has_many :followed_relations, foreign_key: 'followed_id'
+  end
+  with_options class_name: 'User' do
+    has_many :followings, through: :follow_relations, source: :followed
+    has_many :followers, through: :followed_relations, source: :follower
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe User, type: :model do
 
   context 'association' do
     it { is_expected.to have_many(:sleep_cycles).dependent(:destroy) }
+    # to follow relation
+    it { is_expected.to have_many(:follow_relations).class_name('Following').with_foreign_key('follower_id').dependent(:destroy) }
+    it { is_expected.to have_many(:followings).class_name('User').through(:follow_relations).source(:followed) }
+    # to be followed by relation
+    it { is_expected.to have_many(:followed_relations).class_name('Following').with_foreign_key('followed_id').dependent(:destroy) }
+    it { is_expected.to have_many(:followers).class_name('User').through(:followed_relations).source(:follower) }
   end
 end
