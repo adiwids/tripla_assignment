d40938d7d430a786cd004ecaed13fd331d4f75ec
Adi Widyawan <day4die@gmail.com>
2023-04-13 15:45:34 +0700

    Add clock-in service class.

---
+++ app/services/sleep_clock_in_service.rb
@@ -0,0 +1,22 @@
+class SleepClockInService
+  attr_reader :user, :object
+
+  def self.call(user:, set_wake_up_time:)
+    new(user).start(set_wake_up_time)
+  end
+
+  def initialize(user)
+    raise ArgumentError.new unless user.persisted?
+
+    @user = user
+  end
+
+  def start(set_wake_up_time)
+    raise StandardError.new if user.sleep_cycles.active.exists?
+
+    @object = user.sleep_cycles.create(set_wake_up_time: set_wake_up_time, date: set_wake_up_time.try(:to_date))
+    object.active! if object.errors.empty?
+
+    self
+  end
+end
---
+++ spec/services/sleep_clock_in_service_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe SleepClockInService, type: :service do
+  describe '.call' do
+    let(:subject) { described_class.call(user: user, set_wake_up_time: set_wake_up_time) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:set_wake_up_time) { Time.now + 6.hours }
+
+    it 'returns service instance' do
+      expect(subject).to be_an_instance_of(described_class)
+    end
+
+    context 'with valid user and wake up time' do
+      it 'creates active sleep cycle for user' do
+        expect(user.sleep_cycles.active).to be_empty
+        expect { subject }.to change { user.sleep_cycles.active.count }.from(0).to(1)
+        expect(subject.object).to be_persisted
+        expect(subject.object.date).to eq(subject.object.set_wake_up_time.to_date)
+      end
+
+      context 'when another active sleep cycle exist' do
+        before { FactoryBot.create(:sleep_cycle, :active, user: user) }
+
+        it { expect { subject }.to raise_error(StandardError) }
+      end
+    end
+
+    context 'with not persisted user' do
+      let(:user) { FactoryBot.build(:user) }
+
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context 'without giving set wake up time' do
+      let(:set_wake_up_time) { nil }
+
+      it 'does not create sleep cycle record and has error' do
+        expect { subject }.not_to change { user.sleep_cycles.count }
+        expect(subject.object).not_to be_persisted
+        expect(subject.object.errors.key?(:set_wake_up_time)).to be_truthy
+        expect(subject.object.errors.full_messages.first).to match(/can't be blank/)
+      end
+    end
+  end
+end
