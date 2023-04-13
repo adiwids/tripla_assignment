3009794f6eed1e6e66b8a65a4620583cbac40a6e
Adi Widyawan <day4die@gmail.com>
2023-04-13 16:39:31 +0700

    Fix sleep cycle trait, add waken up trait and aliased to inactive.

--- spec/factories/sleep_cycles.rb
+++ spec/factories/sleep_cycles.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     association :user
     date { Time.zone.now.to_date }
     set_wake_up_time { Time.zone.now + 8.hours }
-    actual_wake_up_time { Time.zone.now + 7.hours }
+    actual_wake_up_time { nil }
     duration_miliseconds { 0 }
 
     inactive
@@ -12,8 +12,13 @@ FactoryBot.define do
       status { :active }
     end
 
-    trait :inactive do
+    trait :waken_up do
       status { :inactive }
+      actual_wake_up_time { Time.zone.now + 7.hours }
+    end
+
+    trait :inactive do
+      waken_up
     end
   end
 end
