d8038bf2188fa0b4a5b4a152622e7874a5e3bffa
Adi Widyawan <day4die@gmail.com>
2023-04-15 02:59:33 +0700

    Add API parent controller and add token authentication callback.

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  class BaseController < ActionController::Base
+    before_action :authenticate_token!
+
+    private
+
+    def authenticate_token!
+      @current_user = authenticate_with_http_token do |token, options|
+        begin
+          AuthenticateTokenService.call(token: token)
+        rescue ArgumentError => _error
+          # TODO: convert any exception to return HTTP 401
+          nil
+        end
+      end
+      logger.info "Accessing as #{@current_user&.name}" if Rails.env.development?
+
+      unless @current_user
+        respond_to do |format|
+          format.json do
+            render json: { message: 'unauthorized' }, status: :unauthorized
+          end
+        end
+      end
+    end
+  end
+end
