3cbcfc8af31ff90a6835db7aab2ad3e76f7c635f
Adi Widyawan <day4die@gmail.com>
2023-04-15 01:48:01 +0700

    Add approved following only relations to return actual followed and follower users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :sleep_cycles, dependent: :destroy
-  with_options(class_name: 'Following', dependent: :destroy) do
-    has_many :follow_relations, foreign_key: 'follower_id'
-    has_many :followed_relations, foreign_key: 'followed_id'
+  with_options(class_name: 'Following') do
+    has_many :follow_relations, foreign_key: 'follower_id', dependent: :destroy
+    has_many :accepted_following_requests, -> { where(status: 'approved') }, foreign_key: 'follower_id'
+    has_many :followed_relations, foreign_key: 'followed_id', dependent: :destroy
+    has_many :accepted_being_followed_requests, -> { where(status: 'approved') }, foreign_key: 'followed_id'
   end
   with_options class_name: 'User' do
-    has_many :followings, through: :follow_relations, source: :followed
-    has_many :followers, through: :followed_relations, source: :follower
+    has_many :followings, through: :accepted_following_requests, source: :followed
+    has_many :followers, through: :accepted_being_followed_requests, source: :follower
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,9 +11,11 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:sleep_cycles).dependent(:destroy) }
     # to follow relation
     it { is_expected.to have_many(:follow_relations).class_name('Following').with_foreign_key('follower_id').dependent(:destroy) }
-    it { is_expected.to have_many(:followings).class_name('User').through(:follow_relations).source(:followed) }
+    it { is_expected.to have_many(:accepted_following_requests).class_name('Following').with_foreign_key('follower_id').conditions(status: 'approved') }
+    it { is_expected.to have_many(:followings).class_name('User').through(:accepted_following_requests).source(:followed) }
     # to be followed by relation
     it { is_expected.to have_many(:followed_relations).class_name('Following').with_foreign_key('followed_id').dependent(:destroy) }
-    it { is_expected.to have_many(:followers).class_name('User').through(:followed_relations).source(:follower) }
+    it { is_expected.to have_many(:accepted_being_followed_requests).class_name('Following').with_foreign_key('followed_id').conditions(status: 'approved') }
+    it { is_expected.to have_many(:followers).class_name('User').through(:accepted_being_followed_requests).source(:follower) }
   end
 end
