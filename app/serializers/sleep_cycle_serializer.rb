9d3586c09d080ca191ca59a3b7cd80697b1b5bbe
Adi Widyawan <day4die@gmail.com>
2023-04-15 12:18:08 +0700

    Add creation timestamp to JSON data.

--- app/serializers/sleep_cycle_serializer.rb
+++ app/serializers/sleep_cycle_serializer.rb
@@ -1,7 +1,11 @@
 class SleepCycleSerializer
   include JSONAPI::Serializer
 
-  attributes :set_wake_up_time, :actual_wake_up_time, :duration_miliseconds, :status
+  attributes :set_wake_up_time,
+             :actual_wake_up_time,
+             :duration_miliseconds,
+             :status,
+             :created_at
 
   belongs_to :user
 end
