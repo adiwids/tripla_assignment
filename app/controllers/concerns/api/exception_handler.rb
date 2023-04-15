5b9119f3b7b4b6d4baf73bc4eb2a6bf37612cd11
Adi Widyawan <day4die@gmail.com>
2023-04-15 13:35:44 +0700

    Add HTTP errors classifier to be rescued from service object and handle it with proper JSON error response.

--- app/controllers/concerns/api/exception_handler.rb
+++ app/controllers/concerns/api/exception_handler.rb
@@ -3,14 +3,30 @@ module Api::ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found_error
+    rescue_from HttpError::ForbiddenError, with: :handle_forbidden_error
+    rescue_from ActionController::ParameterMissing, HttpError::BadRequestError, with: :handle_bad_request_error
   end
 
   protected
 
   # TODO: convert exception message to be more comprehensive
   def handle_not_found_error(error)
+    render_json_error error, :not_found
+  end
+
+  def handle_forbidden_error(error)
+    render_json_error error, :forbidden
+  end
+
+  def handle_bad_request_error(error)
+    render_json_error error, :bad_request
+  end
+
+  private
+
+  def render_json_error(error, http_status)
     respond_to do |format|
-      format.json { render json: { message: 'Resource not found' }, status: :not_found }
+      format.json { render json: { message: error.message }, status: http_status }
     end
   end
 end
