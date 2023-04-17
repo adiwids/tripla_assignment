43c5ada490359ff7724541be4bd216645f1e005f
Adi Widyawan <day4die@gmail.com>
2023-04-17 09:04:49 +0700

    Add attribute condition to check following status.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,11 +3,11 @@ class UserSerializer
 
   attributes :name
 
-  attribute :is_following do |object, params|
+  attribute :is_following, if: Proc.new { |object, params| params && params.key?(:follower_ids) } do |object, params|
     params[:follower_ids].include?(object.id)
   end
 
-  attribute :is_followed do |object, params|
+  attribute :is_followed, if: Proc.new { |object, params| params && params.key?(:followed_ids) } do |object, params|
     params[:followed_ids].include?(object.id)
   end
 end
