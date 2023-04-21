5a2a81fabde982a418272a18e3009c9c09be03a3
Adi Widyawan <day4die@gmail.com>
2023-04-21 14:17:26 +0700

    Add module to include pagination into JSON API response.

---
+++ app/controllers/concerns/api/paginated_collection.rb
@@ -0,0 +1,32 @@
+module Api::PaginatedCollection
+  extend ActiveSupport::Concern
+
+  included do
+    include Pagy::Backend
+  end
+
+  protected
+
+  def build_paginator_for(collection)
+    paginator.call(collection, params: page_params, base_url: request.url)
+  end
+
+  def page_params
+    params.require(:page).permit(:size, :number)
+  end
+
+  def paginator
+    @paginator ||= JSOM::Pagination::Paginator.new
+  end
+
+  def paginate(collection, serializer_class, additional_options = {})
+    paginated_collection = build_paginator_for(collection)
+    options = {
+      is_collection: true,
+      meta: paginated_collection.meta.to_h,
+      links: paginated_collection.links.to_h
+    }
+
+    serializer_class.new(paginated_collection.items, options.merge(additional_options))
+  end
+end
