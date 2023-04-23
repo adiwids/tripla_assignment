c22295fe2888b226e8217da55a1fe393db83cf34
Adi Widyawan <day4die@gmail.com>
2023-04-23 15:18:52 +0700

    Enable Resource Caching on sleep cycle and user resources.

--- app/serializers/sleep_cycle_serializer.rb
+++ app/serializers/sleep_cycle_serializer.rb
@@ -1,6 +1,8 @@
 class SleepCycleSerializer
   include JSONAPI::Serializer
 
+  cache_options store: Rails.cache, namespace: 'sleepcycles-jsonapi', expires_in: 8.hours
+
   attributes :set_wake_up_time,
              :actual_wake_up_time,
              :duration_seconds,
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
 
+  cache_options store: Rails.cache, namespace: 'sleepcycles-user-jsonapi', expires_in: 1.hour
+
   attributes :name
 
   attribute :is_following, if: Proc.new { |object, params| params && params.key?(:follower_ids) } do |object, params|
